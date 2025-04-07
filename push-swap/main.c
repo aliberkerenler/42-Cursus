94a1da372c5d70b98278a2db744a5e57110a489d
Berke <aliberkerenler@gmail.com>
2025-04-07 17:46:31 +0300

    Add files via upload

---
+++ push-swap/main.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerenler <aerenler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 17:41:52 by aerenler          #+#    #+#             */
+/*   Updated: 2025/04/07 17:41:56 by aerenler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	fill_stack(t_list **stack, int argc, char **argv)
+{
+	t_list	*new;
+	char	**args;
+	int		i;
+
+	i = 0;
+	if (argc == 2)
+		args = ft_split(argv[1], ' ');
+	else
+	{
+		i = 1;
+		args = argv;
+	}
+	while (args[i])
+	{
+		new = new_item(ft_atoi(args[i]));
+		add_back(stack, new);
+		i++;
+	}
+	index_stack(stack);
+	if (argc == 2)
+		ft_free(args);
+}
+
+static void	sort_stack(t_list **stack_a, t_list **stack_b)
+{
+	if (size(*stack_a) <= 5)
+		simple_sort(stack_a, stack_b);
+	else
+		radix_sort(stack_a, stack_b);
+}
+
+int	main(int argc, char **argv)
+{
+	t_list	**stack_a;
+	t_list	**stack_b;
+
+	if (argc < 2)
+		return (0);
+	check_args(argc, argv);
+	stack_a = (t_list **)malloc(sizeof(t_list));
+	stack_b = (t_list **)malloc(sizeof(t_list));
+	*stack_a = NULL;
+	*stack_b = NULL;
+	fill_stack(stack_a, argc, argv);
+	if (is_sorted(stack_a))
+	{
+		free_stack(stack_a);
+		free_stack(stack_b);
+		return (0);
+	}
+	sort_stack(stack_a, stack_b);
+	free_stack(stack_a);
+	free_stack(stack_b);
+	return (0);
+}
+
---
+++ push-swap/radix.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   radix.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerenler <aerenler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 17:42:47 by aerenler          #+#    #+#             */
+/*   Updated: 2025/04/07 17:42:50 by aerenler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	get_max_bits(t_list **stack)
+{
+	t_list	*head;
+	int		max;
+	int		max_bits;
+
+	head = *stack;
+	max = head->index;
+	max_bits = 0;
+	while (head)
+	{
+		if (head->index > max)
+			max = head->index;
+		head = head->next;
+	}
+	while ((max >> max_bits) != 0)
+		max_bits++;
+	return (max_bits);
+}
+
+void	radix_sort(t_list **stack_a, t_list **stack_b)
+{
+    t_list	*head_a;
+    int		i;
+    int		j;
+    int		stack_size;
+    int		max_bits;
+
+    i = 0;
+    head_a = *stack_a;
+    stack_size = size(head_a);
+    max_bits = get_max_bits(stack_a);
+    while (i < max_bits)
+    {
+        j = 0;
+        while (j++ < stack_size)
+        {
+            head_a = *stack_a;
+            if (((head_a->index >> i) & 1) == 1)
+                ra(stack_a);
+            else
+                pb(stack_a, stack_b);
+        }
+        while (size(*stack_b) != 0)
+            pa(stack_a, stack_b);
+        i++;
+    }
+}
